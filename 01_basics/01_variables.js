e5c43f2bf5b8b6a278a68f84fdac89426e6f74f1
Narayan Mahato <122993980+narayanmahi07@users.noreply.github.com>
2024-12-24 19:12:40 +0000

    update

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail ="naratyan123@google.com"
+var accuntPassword ="12345"
+accountCity = "westbengal"
+let accountstate;
+
+
+accountEmail = "hit@gmail.com"
+accountpassword = "654789"
+accountCity = "delhi"
+
+console.log(accountId);
+/*
+
+prefer not to use var 
+because of issue in block
+*/
+
+console.table([accountId,accountEmail,accuntPassword,accountCity, accountstate]);
