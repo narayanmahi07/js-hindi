befc4fcb1c050ce666c09f073fb10c23fc805df2
Narayan Mahato <122993980+narayanmahi07@users.noreply.github.com>
2024-12-24 19:38:22 +0000

    new update

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version 
+
+// alert(3 + 3)  // we are using nodejs, not browser
+
+console.log(3+3);   // code readability is high 
+console.log("Narayan");
+
+
+let name ="narayan";
+let age = 18;
+let isloggedin = false;
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+
+//boolean => true/false
+//null => standalone value
+// undefine =>
+// symbol => unique    
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
