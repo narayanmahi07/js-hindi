b5a14fb2f91bd3e0777b03719124745d3bf42ef0
Narayan Mahato <122993980+narayanmahi07@users.noreply.github.com>
2024-12-26 12:04:53 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,33 @@
+//let score = "33abc"
+/* let score = undefined */
+let score = null
+
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber =Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" =>NaN
+// TRUE => 1, false => 0
+
+
+let isLoggedIn = "NARAYAN"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+//1 => TRUE; 0=> FALSE;
+// "" => false;
+//"narayan" => true
+
+let someNumber =33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
