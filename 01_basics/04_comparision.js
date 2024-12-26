ea02e1c6498044519c2551279ed4428aab799aa4
Narayan Mahato <122993980+narayanmahi07@users.noreply.github.com>
2024-12-26 12:42:06 +0000

    compare

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,24 @@
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2<1);
+//console.log(2==1);
+//console.log(2 !=1);
+
+console.log("2" > 1);  // string ko number me convert kar dete hai phie compare karte hai 
+console.log("02" > 1);
+
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined < 0);
+
+
+// ===  strict check 
+ 
+console.log("2" === 2);
