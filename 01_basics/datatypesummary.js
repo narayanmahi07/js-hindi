46420219eb363a38ebd26b5a0a0d6c7d1bd9adb2
Narayan Mahato <122993980+narayanmahi07@users.noreply.github.com>
2024-12-26 13:07:18 +0000

    update

--- 01_basics/datatypesummary.js
+++ 01_basics/datatypesummary.js
@@ -41,3 +41,43 @@ console.log(typeof myFunction);
 
 
 console.log(typeof outsideTemp);
+
+
+
+
+
+
+/*   [ data type summary ]
+
+- interview related questions 
+
+- primitive and non primitive (call by value, call by reference)
+
+- primitive:7 (call by value)
+
+- string, number, bolean, null, undefined, symbol , BigInt
+
+- non primitive:3 (call by reference)
+
+- arrays, object, function 
+
+
+- dynamically type vs statically type
+
+- js is dynamically typed
+
+- const id = Symbol("123")
+
+- const anotherId = Symbol("123")
+
+- id === anotherId => false
+
+- array, object, function overview 
+
+- typeof datatyped is available on documentation */
+
+
+/*   [ stack and heap memory ]
+- Primitive data type goes to Stack we get a copy of that value.
+- Non-Primitive data type goes to Heap we get refrence of that value.  
+*/
