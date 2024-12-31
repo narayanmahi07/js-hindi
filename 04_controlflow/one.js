92ea8e5ee5f754cea3df7f2c56ec9f8e180e9fc3
Narayan Mahato <122993980+narayanmahi07@users.noreply.github.com>
2024-12-31 13:21:58 +0000

    if

---
+++ 04_controlflow/one.js
@@ -0,0 +1,61 @@
+// if
+const isUserloggedIn = true
+const temperature = 15
+// if (temperature === 16) {
+ //    console.log("less than 30")
+// }
+// else{
+//     console.log("temperature is greater than 30")
+// }
+
+// console.log("execute");
+// <, >, >= ,<= , ==, != ,=== ,!==
+
+
+
+// const score = 200 
+
+// if (score> 100) {
+//     let power = "fly"
+//     console.log(`user power : ${power}`);
+    
+// }
+// console.log(`user power : ${power}`);
+
+
+
+const balance = 1000
+
+//if (balance > 500) console.log("test"),
+//console.log("bad");                            // it gives the output but it is not a good practice
+    
+
+if (balance < 500) {
+     console.log("less than 500");
+}
+else if (balance < 750) {
+    console.log("less than 750");
+}
+else {
+    console.log("less than 1200");
+      
+}
+
+
+
+
+const UserloggedIn = true
+const debitcard = true 
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (UserloggedIn && debitcard && 2 ==2) {
+    
+     console.log("Allow to buy courses ");
+}
+
+
+if (loggedInFromGoogle ||  loggedInFromEmail) {
+    console.log("user logged in ");
+}
+
