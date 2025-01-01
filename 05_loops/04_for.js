50da6becfd03886cc75df01c2bd7638b3ad29f89
Narayan Mahato <122993980+narayanmahi07@users.noreply.github.com>
2025-01-01 15:32:53 +0000

    for in loop

---
+++ 05_loops/04_for.js
@@ -0,0 +1,37 @@
+const myObject = {
+
+    js: "javascript",
+    cpp: "c++",
+    rb : "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+   // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+
+
+const prog = ["js","rb","py","java","cpp"]
+
+for (const key in prog) {
+    //console.log(prog[key]);
+        
+    }
+
+// +++++ we cannot do iteration on map ++++++//
+
+
+ //   const map  = new Map()
+
+//map.set(`IN`,"INDIA")
+//map.set(`USA`,"UK")
+//map.set(`FR`,"FRANCE")
+//map.set(`IN`,"INDIA")
+
+//for (const key in map) {
+ //   console.log(key);
+        
+  //  }
+
+  
